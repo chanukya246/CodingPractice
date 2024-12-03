8dc7eb7dd02889a42d793cfd696c8c315832215a
chanukya246 <33529876+chanukya246@users.noreply.github.com>
2024-12-03 00:29:13 -0600

    aoc: day 2, challenge 2

---
+++ src/aoc/play/Day2B.java
@@ -0,0 +1,98 @@
+package aoc.play;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day2B {
+
+    public static void main(String[] args) {
+        String filePath = "src/aoc/input/files/day2b_ip.txt"; // Replace with your file path
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int i = 0;
+            int safeReports = 0;
+            while ((line = br.readLine()) != null && i < 1000) {
+                String[] str = line.split(" ");
+                boolean ascending = false;
+                boolean descending = false;
+                if (str.length > 1 && (Integer.parseInt(str[0]) > Integer.parseInt(str[1]))
+                        && Math.abs(Integer.parseInt(str[0]) - Integer.parseInt(str[1])) > 0
+                        && Math.abs(Integer.parseInt(str[0]) - Integer.parseInt(str[1])) < 4) {
+                    descending = true;
+                } else if (str.length > 1 && (Integer.parseInt(str[0]) < Integer.parseInt(str[1]))
+                        && Math.abs(Integer.parseInt(str[0]) - Integer.parseInt(str[1])) > 0
+                        && Math.abs(Integer.parseInt(str[0]) - Integer.parseInt(str[1])) < 4) {
+                    ascending = true;
+                }
+
+                if (ascending) {
+                    ascending = checkIfAscendingSafe(str, ascending);
+                    if (!ascending) {
+                        ascending = checkSafeByRemoval(str, ascending, "ascending");
+                    }
+                } else if (descending) {
+                    descending = checkIfDescendingSafe(str, descending);
+                    if (!descending) {
+                        descending = checkSafeByRemoval(str, descending, "descending");
+                    }
+                }
+                i++;
+                if (ascending || descending) {
+                    safeReports++;
+                }
+            }
+            System.out.println("safeReports: " + safeReports);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkIfDescendingSafe(String[] str, boolean descending) {
+        for (int j = 1; j < str.length; j++) {
+            int prev = Integer.parseInt(str[j - 1]);
+            int curr = Integer.parseInt(str[j]);
+            int abs = Math.abs(curr - prev);
+            if (!((prev > curr) && abs > 0 && abs < 4)) {
+                descending = false;
+                break;
+            }
+        }
+        return descending;
+    }
+
+    private static boolean checkIfAscendingSafe(String[] str, boolean ascending) {
+        for (int j = 1; j < str.length; j++) {
+            int prev = Integer.parseInt(str[j - 1]);
+            int curr = Integer.parseInt(str[j]);
+            int abs = Math.abs(curr - prev);
+            if (!((prev < curr) && abs > 0 && abs < 4)) {
+                ascending = false;
+                break;
+            }
+        }
+        return ascending;
+    }
+
+    private static boolean checkSafeByRemoval(String[] str, boolean ascOrDescSafeFlag, String ascOrDesc) {
+        for (int i = 0; i < str.length; i++) {
+            List<String> list = new ArrayList<>();
+            list.addAll(Arrays.stream(str).toList());
+            list.remove(i);
+            if (ascOrDesc.equalsIgnoreCase("ascending")) {
+                ascOrDescSafeFlag = checkIfAscendingSafe(list.toArray(new String[0]), !ascOrDescSafeFlag);
+            } else if (ascOrDesc.equalsIgnoreCase("descending")) {
+                ascOrDescSafeFlag = checkIfDescendingSafe(list.toArray(new String[0]), !ascOrDescSafeFlag);
+            }
+            if (ascOrDescSafeFlag) {
+                break;
+            }
+        }
+        return ascOrDescSafeFlag;
+    }
+
+}
---
+++ src/aoc/play/Day2B_try2.java
@@ -0,0 +1,85 @@
+package aoc.play;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day2B_try2 {
+
+    public static boolean checkSafe(List<String> numStr) {
+        int currentNum = -1;
+        int inc = 0;
+        boolean safe = true;
+        for (String num : numStr) {
+            int number = Integer.parseInt(num);
+            if (currentNum == -1) {
+                currentNum = number;
+                continue;
+            }
+            if (currentNum > number) {
+                if (inc > 0) {
+                    safe = false;
+                    break;
+                } else if (inc == 0) {
+                    inc = -1;
+                }
+                if (Math.abs(currentNum - number) > 3) {
+                    safe = false;
+                    break;
+                }
+                currentNum = number;
+            } else if (currentNum < number) {
+                if (inc < 0) {
+                    safe = false;
+                    break;
+                } else if (inc == 0) {
+                    inc = 1;
+                }
+                if (Math.abs(currentNum - number) > 3) {
+                    safe = false;
+                    break;
+                }
+                currentNum = number;
+            } else {
+                safe = false;
+                break;
+            }
+        }
+        return safe;
+    }
+
+    public static void main(String[] args) throws IOException  {
+
+        String filePath = "src/aoc/input/files/day2a_ip.txt"; // Replace with your file path
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int ii = 0;
+            int num_safe = 0;
+            while ((line = br.readLine()) != null && ii < 1000) {
+                String[] numStr = line.split(" ");
+
+                List<String> numList = new ArrayList<>();
+                numList.addAll(Arrays.stream(numStr).toList());
+                boolean safe = checkSafe(numList);
+                if (!safe) {
+                    for (int i = 0; i < numStr.length; i++) {
+                        numList = new ArrayList<>();
+                        numList.addAll(Arrays.stream(numStr).toList());
+                        numList.remove(i);
+                        safe = checkSafe(numList);
+                        if (safe) break;
+                    }
+                    if (!safe) System.out.println(line);
+                }
+                num_safe += safe ? 1 : 0;
+            }
+            System.out.println(num_safe);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
