b381a36d9bcfdab9f02832b1143ac9054202c5c0
chanukya246 <33529876+chanukya246@users.noreply.github.com>
2024-12-11 17:17:38 -0600

    aoc: day (11): challenge 1

---
+++ src/aoc2024/play/Day11.java
@@ -0,0 +1,37 @@
+package aoc2024.play;
+
+import java.math.BigInteger;
+
+public class Day11 {
+
+    static int blinks = 25;
+
+    public static void main(String[] args) {
+        String input = "17639 47 3858 0 470624 9467423 5 188";
+        day11part1(input, blinks);
+    }
+
+    private static void day11part1(String input, int blink) {
+        StringBuilder sb = new StringBuilder();
+        String[] ipSplit = input.split(" ");
+        while (blink > 0) {
+            for (String str : ipSplit) {
+                int strLength = str.trim().toCharArray().length;
+                BigInteger bi = new BigInteger(str.trim());
+                if (bi.compareTo(BigInteger.ZERO) == 0) {
+                    sb.append("1").append(" ");
+                } else if (strLength % 2 == 0) {
+                    sb.append(new BigInteger(str.substring(0, strLength / 2))).append(" ");
+                    sb.append(new BigInteger(str.substring(strLength / 2))).append(" ");
+                } else {
+                    sb.append(bi.multiply(BigInteger.valueOf(2024))).append(" ");
+                }
+            }
+            ipSplit = sb.toString().split(" ");
+            blink--;
+            System.out.println("sb length is : " + ipSplit.length + ", after : " + (blinks - blink) + " blinks");
+            sb.setLength(0);
+        }
+
+    }
+}
